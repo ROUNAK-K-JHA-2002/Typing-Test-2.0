d1ba7c261738896811d4c0cbe3fba6e94a0f8c7b
ROUNAK KUMAR JHA <rounakjha291202.rar@gmail.com>
2022-12-24 12:53:48 +0530

    Minor Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import './App.css'
-
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Login from './Pages/Login/index'
+import Home from './Pages/Home/index'
+
+
 
 function App() {
- 
+
+  // Checking if User is already Logged In 
+  const [user,setuser] = useState('')
+useEffect (()=>{
+  setuser(localStorage.getItem('userDetails'))
+},[])
+
+console.log(user)
 
   return (
-    <div className="App">
-     <Login/>
-    </div>
+    // <div className="App">
+    //  <Login/>
+    // </div>
+    <Routes>
+				<Route
+					exact
+					path="/"
+					element={user ? <Home user={user} /> : <Navigate to="/login" />}
+				/>
+				<Route
+					exact
+					path="/login"
+					element={user ? <Navigate to="/" /> : <Login />}
+				/>
+			</Routes>
   )
 }
 
