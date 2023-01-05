1218b0cdf4476e72ddbfb69b3e4e7c47e22886e8
ROUNAK KUMAR JHA <rounakjha291202.rar@gmail.com>
2023-01-06 00:23:37 +0530

    Added Header Component

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './header.css'
+function Header() {
+  return (
+    <div className='Header text-white text-center'>Welcome to Typing Rectare</div>
+  )
+}
+
+export default Header
