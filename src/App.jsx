2ca059fdcf39df5188b27b9a5533c03e822ebbcc
ROUNAK KUMAR JHA <rounakjha291202.rar@gmail.com>
2023-01-06 00:24:56 +0530

    Updated App.jsx for Loading Animations

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,27 @@ import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Login from './Pages/Login/index'
 import Home from './Pages/Home/index'
+import Loading from "./Components/Loading";
 
 
 
 function App() {
+	const [loading,setLoading] = useState(true);
 
   // Checking if User is already Logged In 
   const [user,setuser] = useState('')
 useEffect (()=>{
   setuser(localStorage.getItem('userDetails'))
 },[])
+
+useEffect(()=>{
+  const time =  setTimeout(()=>{
+      setLoading(false)
+    },3000)
+    return (()=>{
+      clearInterval(time)
+    })
+   })
 
 console.log(user)
 
@@ -25,12 +36,12 @@ console.log(user)
 				<Route
 					exact
 					path="/"
-					element={user ? <Home user={user} /> : <Navigate to="/login" />}
+					element={loading ? <Loading /> : user ? <Home user={user} /> : <Navigate to="/login" />}
 				/>
 				<Route
 					exact
 					path="/login"
-					element={user ? <Navigate to="/" /> : <Login />}
+					element={loading ? <Loading /> : user ? <Navigate to="/" /> : <Login />}
 				/>
 			</Routes>
   )
